52475b6896a48235e4a5e2c1fd0af8779c4ac1cf
ViniCarvalho71 <carvalhovinicius71@gmail.com>
2022-07-19 00:52:13 -0300

    Commit do exercício de acrescentar o campo local no banco junto com a inserção do mesmo pelo forms

--- core/views.py
+++ core/views.py
@@ -43,10 +43,12 @@ def submit_evento(request):
         titulo = request.POST.get('titulo')
         data_evento = request.POST.get('data_evento')
         desc = request.POST.get('descricao')
+        local = request.POST.get('local')
         usuario = request.user
         Evento.objects.create(titulo=titulo,
                             data_evento=data_evento,
                             descricao=desc,
+                            local=local,
                             usuario=usuario,
                             )
     return redirect('/')
